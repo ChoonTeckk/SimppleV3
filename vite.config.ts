7b51116ca9995624b7aa8e2722db4db6d7b591c5
ChoonTeckk <choonteck@ugenesolutions.com>
2025-08-25 11:50:08 +0800

    first

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +10,31 @@ export default defineConfig({
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate", // automatically update service worker
+      manifest: {
+        name: "TailAdmin Dashboard",
+        short_name: "TailAdmin",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1f2937",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
