70739ba0c73fbb08a55dd25743911a794f5a9147
ChoonTeckk <choonteck@ugenesolutions.com>
2025-09-09 16:29:02 +0800

    fixes: -remove the extra datepicker import

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -4,7 +4,6 @@ import OfflineNotice from "../components/OfflineNotice";
 import "react-datepicker/dist/react-datepicker.css";
 import "../index.css";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 
 
